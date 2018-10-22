67f0c18d69921f8532692e415c29bf6885f3abfe
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-10-22 13:25:58 +0400

    Slack Notifier

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # jenkins-ci-shared-library
 Useful scripts for Jenkins pipelines
+
+## slackNotifier
+params: `buildResult`
+env: `STAGE`, `JOB_NAME`, `BUILD_NUMBER`
---
+++ vars/slackNotifier.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+def call(String buildResult) {
+  if ( buildResult == "SUCCESS" ) {
+    slackSend color: "good", message: "(${env.STAGE}) Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+  }
+  else if( buildResult == "FAILURE" ) { 
+    slackSend color: "danger", message: "(${env.STAGE}) Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "(${env.STAGE}) Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+  }
+  else {
+    slackSend color: "danger", message: "(${env.STAGE}) Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unclear"	
+  }
+}
