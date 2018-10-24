c5cd9a737f8670d39a82372126bcdd83bf4fdabc
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-10-24 17:10:47 +0400

    slack message

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,16 +1,10 @@
 #!/usr/bin/env groovy
-import groovy.json.*
+import groovy.json.JsonBuilder;
 
+def json = new JsonBuilder()
 
 def call(String buildResult) {
-  // Create JSON objects
-  JSONArray attachments = new JSONArray();
-  JSONObject attachment = new JSONObject();
-  JSONArray fields = new JSONArray();
-  JSONObject field_environment = new JSONObject();
-  JSONObject field_status = new JSONObject();
-  JSONObject field_commit = new JSONObject();
-
+  // Create JSON object
   // Initial variables
   job_title = "${env.JOB_NAME} (${env.BUILD_DISPLAY_NAME})"
   color = ""
@@ -26,35 +20,35 @@ def call(String buildResult) {
     status = "FAIL at ${env.STAGE_NAME}"
   }
 
-  // Build up the fields
-  field_environment.put('title', "Environment");
-  field_environment.put('value', env.STAGE);
-  field_environment.put('short', 1);
+  // Build up the object
+  def root = json [
+    fallback "${status}: ${job_title}"
+    color color
+    title job_title
+    title_link env.RUN_DISPLAY_URL
+  ]
+  
+  // field_environment.put('title', "Environment");
+  // field_environment.put('value', env.STAGE);
+  // field_environment.put('short', 1);
 
-  field_status.put('title', "Status");
-  field_status.put('value', status);
-  field_status.put('short', 1);
+  // field_status.put('title', "Status");
+  // field_status.put('value', status);
+  // field_status.put('short', 1);
 
-  field_commit.put('title', "Commit ID");
-  field_commit.put('value', env.GIT_COMMIT);
-  field_commit.put('short', 0);
+  // field_commit.put('title', "Commit ID");
+  // field_commit.put('value', env.GIT_COMMIT);
+  // field_commit.put('short', 0);
 
-  // Add them to the fields array
-  attachments.add(field_environment);
-  attachments.add(field_status);
-  attachments.add(field_commit);
+  // // Add them to the fields array
+  // attachments.add(field_environment);
+  // attachments.add(field_status);
+  // attachments.add(field_commit);
 
-  // Build up the attachment
-  attachment.put('fallback', "${status}: ${job_title}");
-  attachment.put('color', color);
-  attachment.put('title', job_title);
-  attachment.put('title_link', env.RUN_DISPLAY_URL);
-  attachment.put('fields', fields);
+  // // Build up the attachment
+  // attachment.put('fields', fields);
 
-  // Add it to the atachments array
-  attachments.add(attachment);
 
-  print attachments.toString()
 
-  slackSend(channel: '@arnas', attachments: attachments.toString())
+  slackSend(channel: '@arnas', attachments: root.toString())
 }
