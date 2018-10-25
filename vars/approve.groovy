6adc89c3a74e54214c665ff36e5cd1d35e45c855
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-10-25 12:00:58 +0400

    jenkins

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -1,5 +1,7 @@
 def call() {
-  slackSend(channel: 'temp-notification-dev', message: "<!here> Waiting for approval to deploy *${env.SERVICE}* to *${env.STAGE}* environment")
+  def job_title = "${env.SERVICE} (${env.BUILD_DISPLAY_NAME})"
+
+  slackSend(channel: 'temp-notification-dev', message: "<!here> Waiting for approval to deploy *${env.SERVICE}* to *${env.STAGE}* environment\n<${env.RUN_DISPLAY_URL}|${job_title}>")
 
   def FEEDBACK = input message: "Deploy ${env.SERVICE_DIR} to ${env.STAGE}?", submitterParameter: 'submitter', parameters: [choice(name: "Deploy ${env.SERVICE_DIR} to ${env.STAGE}?", choices: 'no\nyes', description: 'Choose "yes" if you want to deploy this build')]
 
@@ -10,3 +12,6 @@ def call() {
     slackSend(channel: 'temp-notification-dev', color: 'good', message: "Deployment of *${env.SERVICE}* to *${env.STAGE}*. Approved by *${FEEDBACK.submitter}*")
   }
 }
+
+
+
