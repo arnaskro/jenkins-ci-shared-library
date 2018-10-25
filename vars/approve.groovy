8c558949c47acfb96cea516e6704dca4411c1050
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-10-25 11:34:30 +0400

    jenkins

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -1,7 +1,10 @@
 def call() {
-  CHOICE = input message: "Deploy ${env.SERVICE_DIR} to ${env.STAGE}?", parameters: [choice(name: "Deploy ${env.SERVICE_DIR} to ${env.STAGE}?", choices: 'no\nyes', description: 'Choose "yes" if you want to deploy this build')]
+  def FEEDBACK = input message: "Deploy ${env.SERVICE_DIR} to ${env.STAGE}?", submitterParameter: 'submitter', parameters: [choice(name: "Deploy ${env.SERVICE_DIR} to ${env.STAGE}?", choices: 'no\nyes', description: 'Choose "yes" if you want to deploy this build')]
 
-  if (CHOICE == 'no') {
+  if (FEEDBACK == 'no') {
     error 'Deployment cancelled.'
+    slackSend(channel: 'temp-notification-dev', color: 'danger', text: "Deployment of ${env.SERVICE} to ${env.STAGE}. Not approved by ${FEEDBACK.submitter}")
+  } else {
+    slackSend(channel: 'temp-notification-dev', color: 'good', text: "Deployment of ${env.SERVICE} to ${env.STAGE}. Approved by ${FEEDBACK.submitter}")
   }
 }
