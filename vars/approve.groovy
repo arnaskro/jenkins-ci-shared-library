3bf99ce0b143bccfc58957acb4f9503e861410c4
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-10-25 11:46:14 +0400

    jenkins

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -1,12 +1,12 @@
 def call() {
-  slackSend(channel: 'temp-notification-dev', text: "<!here> Waiting for approval to deploy *${env.SERVICE}) to ${env.STAGE} environment")
+  slackSend(channel: 'temp-notification-dev', message: "<!here> Waiting for approval to deploy *${env.SERVICE}) to ${env.STAGE} environment")
 
   def FEEDBACK = input message: "Deploy ${env.SERVICE_DIR} to ${env.STAGE}?", submitterParameter: 'submitter', parameters: [choice(name: "Deploy ${env.SERVICE_DIR} to ${env.STAGE}?", choices: 'no\nyes', description: 'Choose "yes" if you want to deploy this build')]
 
   if (FEEDBACK == 'no') {
     error 'Deployment cancelled.'
-    slackSend(channel: 'temp-notification-dev', color: 'danger', text: "Deployment of *${env.SERVICE}* to *${env.STAGE}*. Not approved by *${FEEDBACK.submitter}*")
+    slackSend(channel: 'temp-notification-dev', color: 'danger', message: "Deployment of *${env.SERVICE}* to *${env.STAGE}*. Not approved by *${FEEDBACK.submitter}*")
   } else {
-    slackSend(channel: 'temp-notification-dev', color: 'good', text: "Deployment of *${env.SERVICE}* to *${env.STAGE}*. Approved by *${FEEDBACK.submitter}*")
+    slackSend(channel: 'temp-notification-dev', color: 'good', message: "Deployment of *${env.SERVICE}* to *${env.STAGE}*. Approved by *${FEEDBACK.submitter}*")
   }
 }
