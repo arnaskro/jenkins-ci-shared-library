10441b22c07f9b5ad58aa51e2e6746f2102956e8
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-10-30 09:07:01 +0400

    update

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -3,9 +3,9 @@ def call() {
   echo "Service: ${env.SERVICE_DIR}"
   echo "Stage: ${env.STAGE}"
 
-
   def node = tool name: 'nodejs8', type: 'jenkins.plugins.nodejs.tools.NodeJSInstallation'
   env.PATH = "${node}/bin:${env.PATH}"
+  sh 'npm install'
 
   sh "sls deploy --aws-profile ${env.STAGE} --stage ${env.STAGE}"
   echo "Done!"
