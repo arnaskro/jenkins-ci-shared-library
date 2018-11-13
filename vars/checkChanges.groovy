d169adf8b5374f8a13a1ee6be1116e18a5c88408
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-11-13 14:45:18 +0400

    changes

--- vars/checkChanges.groovy
+++ vars/checkChanges.groovy
@@ -1,8 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(previous_commit, new_commit, service_folder) {
-  return sh (
-    script: "git diff --name-only ${previous_commit} ${new_commit} ${service_folder}",
-    returnStatus: true
-  ) != 0
+  node {
+    return sh (
+      script: "git diff --name-only ${previous_commit} ${new_commit} ${service_folder}",
+      returnStatus: true
+    ) != 0
+  }
 }
