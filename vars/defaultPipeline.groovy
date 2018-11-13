f099a4bce113c17bcaeda4bd95af117346020e2b
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-11-13 14:24:37 +0400

    changes

--- vars/defaultPipeline.groovy
+++ vars/defaultPipeline.groovy
@@ -1,100 +1,100 @@
 #!/usr/bin/env groovy
 
 def run() {
-    pipeline {
-      agent { label 'build-node' }
+  pipeline {
+    agent { label 'build-node' }
 
-      triggers {
-        GenericTrigger(
-          genericVariables: [
-            [key: 'ref', value: '$.ref']
-          ],
-          
-          causeString: 'Triggered on $ref',
-          printContributedVariables: true,
-          printPostContent: true,
-          silentResponse: false,
-          
-          token: 'IJ58saMFRP0p',
-          
-          regexpFilterText: "$ref",
-          regexpFilterExpression: '(refs/heads/(master|development))'
-        )
-      }
+    triggers {
+      GenericTrigger(
+        genericVariables: [
+          [key: 'ref', value: '$.ref']
+        ],
+        
+        causeString: 'Triggered on $ref',
+        printContributedVariables: true,
+        printPostContent: true,
+        silentResponse: false,
+        
+        token: 'IJ58saMFRP0p',
+        
+        regexpFilterText: "$ref",
+        regexpFilterExpression: '(refs/heads/(master|development))'
+      )
+    }
 
-      stages {
-        stage('Initialize') {
-          steps {
-            script {
-              dir(env.SERVICE_DIR) {
-                initialize()
-              }
-            }
-          }
-        }
-
-        stage('Build') {
-          steps {
-            script {
-              dir(env.SERVICE_DIR) {
-                build()
-              }
-            }
-          }
-        }
-
-        stage ('Test') {
-          steps {
-            script {
-              dir(env.SERVICE_DIR) {
-                test()
-              }
-            }
-          }
-        }
-
-        stage('Approve'){
-          when {
-            expression { STAGE ==~ /(production|prerel)/ }
-          }
-          steps {
-            script {
-              dir(env.SERVICE_DIR) {
-                approve()
-              }
-            }
-          }
-        }
-
-        stage('Deploy'){
-          agent { label 'deploy-node' }
-          steps {
-            script {
-              dir(env.SERVICE_DIR) {
-                deploy()
-              }
-            }
-          }
-        }
-      }
-
-      post {
-        always {
+    stages {
+      stage('Initialize') {
+        steps {
           script {
-            try {
-              slackNotifier(currentBuild.currentResult)
-            } catch(e) {
-              // Simply ignore the error, because the slack notification always succeed
-              echo 'Done!'
+            dir(env.SERVICE_DIR) {
+              initialize()
+            }
+          }
+        }
+      }
+
+      stage('Build') {
+        steps {
+          script {
+            dir(env.SERVICE_DIR) {
+              build()
+            }
+          }
+        }
+      }
+
+      stage ('Test') {
+        steps {
+          script {
+            dir(env.SERVICE_DIR) {
+              test()
+            }
+          }
+        }
+      }
+
+      stage('Approve'){
+        when {
+          expression { STAGE ==~ /(production|prerel)/ }
+        }
+        steps {
+          script {
+            dir(env.SERVICE_DIR) {
+              approve()
+            }
+          }
+        }
+      }
+
+      stage('Deploy'){
+        agent { label 'deploy-node' }
+        steps {
+          script {
+            dir(env.SERVICE_DIR) {
+              deploy()
             }
           }
         }
       }
     }
+
+    post {
+      always {
+        script {
+          try {
+            slackNotifier(currentBuild.currentResult)
+          } catch(e) {
+            // Simply ignore the error, because the slack notification always succeed
+            echo 'Done!'
+          }
+        }
+      }
+    }
+  }
 }
 
 def call() {
-  if(checkChanges() {
+  if(checkChanges()) {
     run()
   }
 }
